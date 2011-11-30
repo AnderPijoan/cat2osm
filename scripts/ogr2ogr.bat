007747b8868d7e631b1cb9214bc3ea95ac939f56
AnderPijoan <ander.pijoan@deusto.es>
2011-11-30 11:27:03 +0100

    Pasa los tags de Elemtex a Constru

--- scripts/ogr2ogr.bat
+++ scripts/ogr2ogr.bat
@@ -8,5 +8,4 @@ set GEOTIFF_CSV=%FWTOOLS_DIR%\data
 set GDAL_DATA=%FWTOOLS_DIR%\data
 set GDAL_DRIVER_PATH=%FWTOOLS_DIR%\gdal_plugins
 
-
 ogr2ogr.exe -s_srs "+init=epsg:23030 +nadgrids=./peninsula.gsb +wktext" -t_srs EPSG:4326 %2 %3
