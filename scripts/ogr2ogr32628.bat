7b8c750f3de59e8a954128cf48e8b46e8dc1526c
AnderPijoan <ander.pijoan@deusto.es>
2011-12-20 10:55:30 +0100

    Traduccion lista a falta de Tag MASA y CONSTRUALTURAS

---
+++ scripts/ogr2ogr32628.bat
@@ -0,0 +1,11 @@
+@echo off
+SET FWTOOLS_DIR=C:\PROGRA~2\FWTOOL~1.7
+
+PATH=%FWTOOLS_DIR%\bin;%FWTOOLS_DIR%\python;%PATH%
+set PYTHONPATH=%FWTOOLS_DIR%\pymod
+set PROJ_LIB=%FWTOOLS_DIR%\proj_lib
+set GEOTIFF_CSV=%FWTOOLS_DIR%\data
+set GDAL_DATA=%FWTOOLS_DIR%\data
+set GDAL_DRIVER_PATH=%FWTOOLS_DIR%\gdal_plugins
+
+ogr2ogr.exe -t_srs EPSG:4326 %2 %3
